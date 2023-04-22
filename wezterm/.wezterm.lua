49b18b0441c3490936e3c0ce5b8646e3fff523e6
Sandro Sikic <sandro.sikic@gmail.com>
2023-04-22 23:07:14 +0200

    update

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,55 @@
+local wezterm = require 'wezterm'
+local act = wezterm.action
+local config = {}
+
+config.font = wezterm.font 'FiraCode Nerd Font'
+config.color_scheme = 'Catppuccin Mocha'
+
+config.keys = {
+  { 
+    key = 'v', 
+    mods = 'CTRL', 
+    action = act.PasteFrom 'Clipboard' 
+  },
+  {
+    key = 'c',
+    mods = 'CTRL',
+    action = wezterm.action.CopyTo 'ClipboardAndPrimarySelection',
+  },
+}
+
+config.colors = {
+  tab_bar = {
+    background = '#0b0022',
+    active_tab = {
+      bg_color = '#2b2042',
+      fg_color = '#c0c0c0',
+      intensity = 'Normal',
+      underline = 'None',
+      italic = false,
+      strikethrough = false,
+    },
+    inactive_tab = {
+      bg_color = '#1b1032',
+      fg_color = '#808080',
+    },
+    inactive_tab_hover = {
+      bg_color = '#3b3052',
+      fg_color = '#909090',
+      italic = true,
+    },
+    new_tab = {
+      bg_color = '#1b1032',
+      fg_color = '#808080',
+    },
+    new_tab_hover = {
+      bg_color = '#3b3052',
+      fg_color = '#909090',
+      italic = true,
+    },
+  },
+}
+
+return config
+
+
