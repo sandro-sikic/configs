724011474bdde8ff46ea773cca14fb047f286d91
Sandro Sikic <sandro.sikic@gmail.com>
2023-11-02 03:26:28 +0100

    update

--- portainer/stacks/static_host/entrypoint.sh
+++ portainer/stacks/static_host/entrypoint.sh
@@ -1,7 +1,1 @@
-#!/bin/bash
-
-rm -r /download
-mkdir /download
-rm -r /data/*
-wget $ZIPPED_SITE_LINK -O /download/site.zip
-unzip /download/site.zip -d /data
+rm -r /download; mkdir /download; rm -r /data/*; wget $ZIPPED_SITE_LINK -O /download/site.zip; unzip /download/site.zip -d /data
