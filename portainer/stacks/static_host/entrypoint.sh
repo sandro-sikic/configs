e2d858d0f6961b7cf4c4dbf9f2caa29ce2d1891d
Sandro Sikic <sandro.sikic@gmail.com>
2023-11-02 03:08:05 +0100

    update

--- portainer/stacks/static_host/entrypoint.sh
+++ portainer/stacks/static_host/entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-export ZIPPED_SITE_LINK="https://atech.itsolutionsdevelopment.xyz/wp-content/uploads/simply-static/temp-files/simply-static-1-1698854297.zip"
-
 rm -r /download
 mkdir /download
 rm -r /data/*
